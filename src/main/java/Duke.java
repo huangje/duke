8df7fe7a0500596bca363ed4c812cf58f6af89de
huangje <jerome.huang@grenoble-inp.org>
2019-08-22 05:51:02 +0200

    Level 2 Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,16 +10,28 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         String line = "______________________________________________________________ \n";
+        String[] tasks = new  String[100];
+        int tasksId = 0;
         boolean running = true;
         while(running){
             Scanner input = new Scanner(System.in);
             String message = input.nextLine();
-            if(message.equals("bye")){
+            if(message.equals("bye")){ //exit
                 System.out.println(line + "bye. Hope to see you again soon ! \n" + line);
                 running = false;
             }
+            else if(message.equals("list")){ //listing all tasks
+                System.out.print(line);
+                for(int i =0; i <tasksId; i++){
+                    int numbTask = i+1;
+                    System.out.println(numbTask + ". " + tasks[i]);
+                }
+                System.out.print(line);
+            }
             else {
-                System.out.println(line + message + "\n" +line);
+                tasks[tasksId] = message; //echo the task
+                tasksId++;
+                System.out.println(line + "added :" + message + "\n" +line);
             }
         }
     }
