357bdd5d975c0a5ea11b24513d016d6952212dca
huangje <jerome.huang@grenoble-inp.org>
2019-08-22 04:51:32 +0200

    level-1 Great, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,6 @@
+import java.util.*;
+
+//@@author huangje
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +9,18 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        String line = "______________________________________________________________ \n";
+        boolean running = true;
+        while(running){
+            Scanner input = new Scanner(System.in);
+            String message = input.nextLine();
+            if(message.equals("bye")){
+                System.out.println(line + "bye. Hope to see you again soon ! \n" + line);
+                running = false;
+            }
+            else {
+                System.out.println(line + message + "\n" +line);
+            }
+        }
     }
 }
