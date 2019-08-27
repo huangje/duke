e9a5b0cd9b6cdb2b874dde75b8cb21fbd99fc0f5
huangje <jerome.huang@grenoble-inp.org>
2019-08-27 22:06:58 +0800

    simplify the exception
    one big block of try catch

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -43,126 +43,95 @@ public class Duke {
                 System.out.print(line);
             }
             else {
-                if(!(tokens[0].equals("todo") || tokens[0].equals("deadline") || tokens[0].equals("event"))){
-                    try{
-                        throw new NoCommandException(line + "OOPS!!! I'm sorry, but I don't know what that means :-(\n"+line);
-                    }catch(NoCommandException e){
-                        System.out.println(e);
+                try {
+                    if (!(tokens[0].equals("todo") || tokens[0].equals("deadline") || tokens[0].equals("event"))) {
+                        throw new NoCommandException(line + "OOPS!!! I'm sorry, but I don't know what that means :-(\n" + line);
                     }
-                }
-                else {
-                    if(tokens[0].equals("todo")){
-                        String[] todoMessage = message.split("todo");
-                        if(todoMessage.length == 0) {
-                            try {
-                                throw new NoTaskException(line + "OOPS!!! The description of a todo cannot be empty\n" + line);
-                            }catch(NoTaskException e){
-                                System.out.println(e) ;
+                    else {
+                        if (tokens[0].equals("todo")) {
+                            String[] todoMessage = message.split("todo");
+                            if (todoMessage.length == 0) {
+                                    throw new NoTaskException(line + "OOPS!!! The description of a todo cannot be empty\n" + line);
+
                             }
-                        }
-                        else {
-                            tasks[tasksId] = new Todo(todoMessage[1].trim());
-                            int numberTask = tasksId + 1;
-                            System.out.println(line + "Got it. I've added this task : \n" + tasks[tasksId].toString() +
-                                    "\nNow you have " + numberTask + " tasks in the list. \n" +line);
-                            tasksId++;
-                        }
+                            else {
+                                tasks[tasksId] = new Todo(todoMessage[1].trim());
+                                int numberTask = tasksId + 1;
+                                System.out.println(line + "Got it. I've added this task : \n" + tasks[tasksId].toString() +
+                                        "\nNow you have " + numberTask + " tasks in the list. \n" + line);
+                                tasksId++;
+                            }
 
-                    }
-                    else if(tokens[0].equals("deadline")){
+                        }
+                        else if (tokens[0].equals("deadline")) {
 
-                        String[] messageWoDeadline = message.split("deadline");
-                        if(messageWoDeadline.length == 0) {
-                            try {
+                            String[] messageWoDeadline = message.split("deadline");
+                            if (messageWoDeadline.length == 0) {
                                 throw new NoTaskException(line + "OOPS!!! The description of a deadline cannot be empty\n" + line);
-                            }catch(NoTaskException e){
-                                System.out.println(e);
+
                             }
-                        }
-                        else {
-                            String[] deadlineMessage =  messageWoDeadline[1].split("by");
-                            if(deadlineMessage.length == 1) {
-                                try {
+                            else {
+                                String[] deadlineMessage = messageWoDeadline[1].split("by");
+                                if (deadlineMessage.length == 1) {
                                     throw new NoDateException(line + "OOPS!!! The date of a deadline cannot be empty\n" + line);
-                                }catch(NoDateException e){
-                                    System.out.println(e) ;
                                 }
-                            }
-                            else {
-                                if(deadlineMessage[0].isBlank()){
-                                    try {
+                                else {
+                                    if (deadlineMessage[0].isBlank()) {
                                         throw new NoTaskException(line + "OOPS!!! The description of a deadline cannot be empty\n" + line);
-                                    }catch(NoTaskException e){
-                                        System.out.println(e);
+                                    }
+                                    else if (deadlineMessage[1].isBlank()) {
+                                        throw new NoDateException(line + "OOPS!!! The date of a deadline cannot be empty\n" + line);
+                                    }
+                                    else {
+                                        tasks[tasksId] = new Deadline(deadlineMessage[0].trim(), deadlineMessage[1].trim());
+                                        int numberTask = tasksId + 1;
+                                        System.out.println(line + "Got it. I've added this task : \n" + tasks[tasksId].toString() +
+                                                "\nNow you have " + numberTask + " tasks in the list. \n" + line);
+                                        tasksId++;
                                     }
                                 }
-                                else if(deadlineMessage[1].isBlank()){
-                                    try {
-                                        throw new NoTaskException(line + "OOPS!!! The date of a deadline cannot be empty\n" + line);
-                                    }catch(NoTaskException e){
-                                        System.out.println(e);
-                                    }
-                                }
-                                else {
-                                    tasks[tasksId] = new Deadline(deadlineMessage[0].trim(), deadlineMessage[1].trim());
-                                    int numberTask = tasksId + 1;
-                                    System.out.println(line + "Got it. I've added this task : \n" + tasks[tasksId].toString() +
-                                            "\nNow you have " + numberTask + " tasks in the list. \n" +line);
-                                    tasksId++;
-                                }
+
                             }
 
-                        }
-
-                    }
-                    else if(tokens[0].equals("event")){
-                        String[] messageWoEvent = message.split("event");
-                        if(messageWoEvent.length == 0) {
-                            try {
+                        } else if (tokens[0].equals("event")) {
+                            String[] messageWoEvent = message.split("event");
+                            if (messageWoEvent.length == 0) {
                                 throw new NoTaskException(line + "OOPS!!! The description of a event cannot be empty\n" + line);
-                            }catch(NoTaskException e){
-                                System.out.println(e);
-                            }
-                        }
-                        else {
-
-                            String[] eventMessage =  messageWoEvent[1].split("at");
-                            if(eventMessage.length == 1) {
-                                try {
-                                    throw new NoDateException(line + "OOPS!!! The date of a event cannot be empty\n" + line);
-                                }catch(NoDateException e){
-                                    System.out.println(e) ;
-                                }
                             }
                             else {
-                                if(eventMessage[0].isBlank()){
-                                    try {
-                                        throw new NoTaskException(line + "OOPS!!! The description of a event cannot be empty\n" + line);
-                                    }catch(NoTaskException e){
-                                        System.out.println(e);
-                                    }
-                                }
-                                else if(eventMessage[1].isBlank()){
-                                    try {
-                                        throw new NoTaskException(line + "OOPS!!! The date of a event cannot be empty\n" + line);
-                                    }catch(NoTaskException e){
-                                        System.out.println(e);
-                                    }
+                                String[] eventMessage = messageWoEvent[1].split("at");
+                                if (eventMessage.length == 1) {
+                                    throw new NoDateException(line + "OOPS!!! The date of a event cannot be empty\n" + line);
                                 }
                                 else {
-                                    tasks[tasksId] = new Event(eventMessage[0].trim(), eventMessage[1].trim());
-                                    int numberTask = tasksId + 1;
-                                    System.out.println(line + "Got it. I've added this task : \n" + tasks[tasksId].toString() +
-                                            "\nNow you have " + numberTask + " tasks in the list. \n" +line);
-                                    tasksId++;
+                                    if (eventMessage[0].isBlank()) {
+                                        throw new NoTaskException(line + "OOPS!!! The description of a event cannot be empty\n" + line);
+                                    }
+                                    else if (eventMessage[1].isBlank()) {
+                                        throw new NoDateException(line + "OOPS!!! The date of a event cannot be empty\n" + line);
+                                    }
+                                    else {
+                                        tasks[tasksId] = new Event(eventMessage[0].trim(), eventMessage[1].trim());
+                                        int numberTask = tasksId + 1;
+                                        System.out.println(line + "Got it. I've added this task : \n" + tasks[tasksId].toString() +
+                                                "\nNow you have " + numberTask + " tasks in the list. \n" + line);
+                                        tasksId++;
+                                    }
                                 }
+
                             }
-
                         }
-                    }
 
+                    }
+                }catch (NoCommandException e) {
+                    System.out.println(e);
+                }catch (NoTaskException e) {
+                    System.out.println(e);
+                }catch (NoDateException e) {
+                    System.out.println(e);
                 }
             }
+
         }
     }
 }
